106813769251dd1aafa6f8787db4d3857fae5b14
dianaEigner <diana08041997@gmail.com>
2023-11-11 22:49:55 +0200

    first commit

---
+++ dev/assets/js/custom.js
@@ -0,0 +1,28 @@
+//   let nowYear = 2023;
+//   let year = prompt('Який зараз рік?',"");
+
+// if (year < nowYear){
+//     alert("Ні, трошки більше")
+// }
+// else if (year > nowYear){
+//     alert("Ні, трошки менше")
+// }
+// else {
+//     alert("Все вірно, зараз 2023рік")
+// }
+
+// let ageQuestion = prompt('Введи свою дату народження',"");
+// let lastAge = 1900;
+
+// if (ageQuestion > nowYear){
+//     alert("Поле заповнено невірно, такий рік буде пізніше")
+// }
+// else if(ageQuestion == nowYear){
+//     alert("Вітаю! Успішно зареєстровано новонародженного!")
+// }
+// else if(ageQuestion <= lastAge){
+//     alert("Уфф, так ти мумія?")
+// }
+// else{
+//     alert("Реєстрація успішна")
+// }
